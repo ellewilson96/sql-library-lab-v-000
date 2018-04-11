949244f5385904a319bea27d4b5f17bd2ee6e59f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-11 21:45:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,13 +15,27 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors JOIN series
   ON series.author_id = authors_id
   JOIN subgenres
-  ON series.subgenre.id = subgenres.id"
+  ON series.subgenre.id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series 
+  JOIN books
+  ON books.series_id = series.id
+  JOIN character_books
+  ON character_books.book_id = books.id
+  JOIN characters 
+  ON character_books.character_id = characters.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) as book_count
+    FROM character_books 
+    JOIN characters 
+    ON character_books.character_id = characters.id 
+    GROUP BY characters.name 
+    ORDER BY book_count DESC;"
 end
